4e60770b2f6d4b2428e6b687173132c2989835d9
Aysin <aysinn.akpinar@gmail.com>
2025-03-26 09:56:04 +0000

    Delete 05_js_project_exercises_challenges directory

--- 05_js_project_exercises_challenges/02_multiply.test.js
+++
@@ -1,8 +0,0 @@
-const multiply = require('./02_multiply');
-
-describe('multiply', () => {
-    it('multiplies 3 and 5 returns 15', () => 
-        {
-        expect(multiply(3,5)).toEqual(15);
-    });
-});
