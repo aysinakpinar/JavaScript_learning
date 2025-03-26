e3672c06a81db342724cc8b9035bac363d00b4f9
Aysin <aysinn.akpinar@gmail.com>
2025-03-26 09:56:53 +0000

    Delete 11_javascript_challenges_build_and_test_an_express_server directory

--- 11_javascript_challenges_build_and_test_an_express_server/Express-Simple-Server-JS-Fundamentals/index.js
+++
@@ -1,7 +0,0 @@
-const { app } = require("./app.js");
-
-const port = 3000; //defining what port the application should use 
-
-app.listen(port, () => {
-    console.log("Now listening on port", port);
-});
